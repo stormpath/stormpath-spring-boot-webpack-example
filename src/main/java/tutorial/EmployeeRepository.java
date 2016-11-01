074680f23b86ed59ef81e7e6e95797c1bdf52157
Matt Raible <matt@raibledesigns.com>
2016-11-01 17:18:21 -0600

    Formatting and fixes after completing tutorial

--- src/main/java/tutorial/EmployeeRepository.java
+++ src/main/java/tutorial/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package tutorial;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Override
+    void delete(Long aLong);
 
 }
